7c23629234155d510f5bfa6d4d3806f4899ddf67
Ashik Meerankutty <ashik9591@gmail.com>
2020-03-26 19:16:52 +0530

    Docs: using normal import for React

--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import  React from 'react'
 import { ChangeEventHandler, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { CommonProps } from '../common';
